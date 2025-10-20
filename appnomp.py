02c81678df0835a3b35b035d142bd52766c49558
AtharvaPatange <164712914+AtharvaPatange@users.noreply.github.com>
2025-10-20 23:33:43 +0530

    UPDATED REQ.TXT

--- appnomp.py
+++ appnomp.py
@@ -922,7 +922,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "app:app",
         host="0.0.0.0",
-        port=8000,
+        port=os.getenv("PORT", 8000),
         reload=True,
         log_level="info"
     )
